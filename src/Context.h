f51ae78a3763d36529cc78bb059e67f215c21835
coburndaniel979 <coburndaniel979@gmail.com>
2026-02-26 08:38:59 -0500

    transfer from lift
    might be missing things, also should submodule debouncer and logging?

---
+++ src/Context.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <SdFat.h>
+#include "Sensors/ASM330.h"
+#include "Sensors/ICM20948.h"
+#include "Sensors/INA219.h"
+#include "Sensors/LSP22.h"
+#include "Sensors/MAX10S.h"
+#include "Servo.h"
+#include "SensorManager/SensorBase.h"
+#include "qmekf-lib/qmekf.h"
+
+struct ASM330Data;
+struct LPS22Data;
+struct ICMData;
+struct MAX10SData;
+struct INA219Data;
+
+struct Context {
+    File logFile;
+    File errorLogFile;
+    File fixedRateLogFile;
+    SdFs sd;
+    bool sdInitialized;
+    bool ekfLooping;
+
+    ASM330 accel;
+    LPS22 baro;
+    ICM20948 mag;
+    MAX10S gps;
+    INA219 curr;
+    
+    Servo airBrakes;
+    StateEstimator estimator;
+};
