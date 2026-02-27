49ec73005ae68b53618d83cfca85324f519ae6ec
coburndaniel979 <coburndaniel979@gmail.com>
2026-02-27 12:28:08 -0500

    current sensor delete

--- src/Context.h
+++ src/Context.h
@@ -3,12 +3,11 @@
 #include <SdFat.h>
 #include "boilerplate/Sensors/Impl/ASM330.h"
 #include "boilerplate/Sensors/Impl/ICM20948.h"
-#include "boilerplate/Sensors/Impl/INA219.h"
-#include "boilerplate/Sensors/Impl/LSP22.h"
+//#include "boilerplate/Sensors/Impl/INA219.h"
+#include "boilerplate/Sensors/Impl/LPS22.h"
 #include "boilerplate/Sensors/Impl/MAX10S.h"
 #include "boilerplate/Servo.h"
-#include "boilerplate/SensorBase.h"
-#include "qmekf-lib/qmekf.h"
+#include "boilerplate/qmekf-lib/qmekf.h"
 
 struct ASM330Data;
 struct LPS22Data;
@@ -28,7 +27,7 @@ struct Context {
     LPS22 baro;
     ICM20948 mag;
     MAX10S gps;
-    INA219 curr;
+    //INA219 curr;
     
     Servo airBrakes;
     StateEstimator estimator;
