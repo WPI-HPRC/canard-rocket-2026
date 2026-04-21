309358f1c668777f05fe206a760147e6269c9f16
Dashiell Elliott <dashiell.elliott@yahoo.com>
2026-04-21 15:53:10 -0400

    make compile and factor out some state stuff

--- src/State.h
+++ src/State.h
@@ -1,31 +1,20 @@
 #pragma once
 
 #include "Context.h"
-#include "debouncer.h"
-
-enum StateID {
-    PRELAUNCH,
-    BOOST,
-    COAST,
-    DROGUE_DESCENT,
-    MAIN_DESCENT,
-    RECOVERY,
-    ABORT,
-    NUM_STATES
-};
-
-struct StateData {
-    long long currentTime;
-    long long deltaTime;
-    long long loopCount;
-    long long startTime;
-    long long lastLoopTime;
-    uint32_t lastAccelReadingTime;
-    uint32_t lastBaroReadingTime;
-    Debouncer baroDebouncer = Debouncer(20);
-    Debouncer accelDebouncer = Debouncer(20);
-    Debouncer velDebouncer = Debouncer(20);
-};
+#ifdef __has_include
+  #if __has_include("states/States.h")
+    #include "states/States.h"
+  #else
+    #include "template_states/States.h"
+  #endif
+#else
+  #include "template_states/States.h"
+#endif
 
 typedef void (*StateInitFunc)(StateData *data);
 typedef StateID (*StateLoopFunc)(StateData *data, Context *ctx);
+
+extern StateInitFunc initFuncs[NUM_STATES];
+extern StateLoopFunc loopFuncs[NUM_STATES];
+
+void initStateMap();
