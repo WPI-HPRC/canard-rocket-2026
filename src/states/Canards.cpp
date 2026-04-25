9d4ed63c502b08a6bd33216101fc70b55cd4f8f4
cbscott <cbscott6163@gmail.com>
2026-04-25 17:01:28 -0400

    killed submodules

--- src/states/Canards.cpp
+++ src/states/Canards.cpp
@@ -8,17 +8,17 @@
 void canardsInit (StateData* data) { }
 
 StateID canardsLoop (StateData* data, Context* ctx) { 
-    if (data->currentTime < 5000) {
-        ctx->c1.writeMicroseconds(CANARD_PLUS_5_DEG);
-        ctx->c2.writeMicroseconds(CANARD_PLUS_5_DEG);
-        ctx->c3.writeMicroseconds(CANARD_MINUS_5_DEG);
-        ctx->c4.writeMicroseconds(CANARD_MINUS_5_DEG);
-    } else {
-        ctx->c1.writeMicroseconds(ZERO);
-        ctx->c2.writeMicroseconds(ZERO);
-        ctx->c3.writeMicroseconds(ZERO);
-        ctx->c4.writeMicroseconds(ZERO);
-    }
+    // if (data->currentTime < 5000) {
+    //     ctx->c1.writeMicroseconds(CANARD_PLUS_5_DEG);
+    //     ctx->c2.writeMicroseconds(CANARD_PLUS_5_DEG);
+    //     ctx->c3.writeMicroseconds(CANARD_MINUS_5_DEG);
+    //     ctx->c4.writeMicroseconds(CANARD_MINUS_5_DEG);
+    // } else {
+    //     ctx->c1.writeMicroseconds(ZERO);
+    //     ctx->c2.writeMicroseconds(ZERO);
+    //     ctx->c3.writeMicroseconds(ZERO);
+    //     ctx->c4.writeMicroseconds(ZERO);
+    // }
 
     return CANARDS;
 }
